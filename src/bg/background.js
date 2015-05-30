42b724f11e6905d3b688094b4eaa4b8bf8125baa
Steffen Müller <steffen@mllrsohn.com>
2015-05-30 14:55:07 +0200

    __wip__

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,9 +1,9 @@
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-    var bkg = chrome.extension.getBackgroundPage();
-    if(request.type === 'xing') {
-        bkg.console.log(request.data);
-        // Save it using the Chrome extension storage API.
-        chrome.storage.sync.set(request.data);
-    }
+// chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+//     var bkg = chrome.extension.getBackgroundPage();
+//     if(request.type === 'xing') {
+//         bkg.console.log(request.data);
+//         // Save it using the Chrome extension storage API.
+//         chrome.storage.sync.set(request.data);
+//     }
 
-});
+// });
